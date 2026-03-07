3ee159a03cc954d7e9fb5354f9974f122234ef93
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2026-03-06 22:59:24 -0300

    swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "./docs/swagger.json";
 import { executionRoutes } from "./routes/execution.routes";
 import { stakingRoutes } from "./modules/liquid-staking/routes/staking.routes";
 
@@ -13,6 +15,11 @@ const PORT = process.env.PORT || 3010;
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "PanoramaBlock API Docs",
+}));
 
 app.use("/execution", executionRoutes);
 app.use("/staking", stakingRoutes);
